dd58cc5a0734bffdd06486d3dda3ee7a1f814a31
Tana Zimmer <tana.m.zimmer@gmail.com>
2020-08-09 16:49:23 -0400

    month count function added to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,21 +2,48 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-budget_data = os.path.join('Resources', 'budget_data.csv')
+bank_data = os.path.join('Resources', 'budget_data.csv')
 
-# Define the function and have it accept the 'daterow' as its sole parameter
-def profit_loss(row):
+# Define the function and have it accept the 'bank_data' as its sole parameter
+def totals(bank_data):
     # For readability, it can help to assign your values to variables with descriptive names
-    date = str(row[0])
-    transactions = long(row[1])
+    month = str(bank_data[0])
+    revnue = int(bank_data[1])
+
 
 # Read in the CSV file
-with open(budget_data, 'r') as csvfile:
+with open(bank_data, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
     header = next(csvreader)
+    month = []
+    revenue = []
+    revenue_diff = []
+
+
+# Count number of months in file
+    for row in csvreader:
+        month.append(row[0])
+    print(len(month))
+
+# Net profits/losses over entire timeframe
+    for row in csvreader:
+        revenue.append(row[1])
+
+# Average the changes of the profits/losses over the entire timeframe
+
+# Find the greatest increase of profit
+
+# List the date and amount for greatest increase of profit
+
+# Find the greatest decrease of profit
+
+# List the date and amount for greatest decrease of profit
+
+
+
+
 
     # Loop through the data
-    for row in csvreader:
+
