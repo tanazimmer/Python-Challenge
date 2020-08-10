72695b1db94e23bc8d431f42dbe54da3019f1256
Tana Zimmer <tana.m.zimmer@gmail.com>
2020-08-10 19:34:43 -0400

    code written - working through errors

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,80 @@
 import os
 import csv
 
+#define variables
+total_months = 0
+net_revenue = 0
+revenue_average = 0
+revenue_change = 0
+month_change = []
+month = []
+month_count = []
+
 # Path to collect data from the Resources folder
 bank_data = os.path.join('Resources', 'budget_data.csv')
-
 # Define the function and have it accept the 'bank_data' as its sole parameter
+
 def totals(bank_data):
-    # For readability, it can help to assign your values to variables with descriptive names
+    # assign your values to variables with descriptive names
     month = str(bank_data[0])
     revenue = int(bank_data[1])
-
 
 # Read in the CSV file
 with open(bank_data, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-    header = next(csvreader)
-    month = []
-    revenue = []
-    revenue_total = []
-    revenue_average = []
 
+    # Don't read the header
+    csvheader = next(csvreader)
+    row = next (csvreader)
+ 
+ # Set variables within rows
+    total_months += 1
+    net_revenue = int(row[1])
+    previous_revenue = int(row[1])
+    greatest_inc = int(row[1])
+    greatest_inc_month = row[0]
+    greatest_dec = int(row[1])
+    greatest_dec_month = row[0]
 
-# Count number of months in file
+# Start forloop
     for row in csvreader:
+# Count number of months in file
         month.append(row[0])
-    print(len(month))
+        month_count = len(month)
 
-# Net profits/losses over entire time frame
-    revenue.append(row[1])
-    revenue_int = map(int, revenue)
-    revenue_total = sum(revenue_int)
-    print(revenue_total)
+    # Net profits/losses over entire time frame
+        net_revenue += int(row[1])
 
-# Average the changes of the profits/losses over the entire timeframe
-    revenue_average = revenue_total / len(month)
-    print(revenue_average)
+    # Find the monthly change values
+        revenue_change = int(row[1]) - previous_revenue
+        month_change.append(revenue_change)
+        month_count.append(row[0])
 
-# Find the greatest increase of profit
+    # Average the changes of the profits/losses over the entire timeframe
+        revenue_average = int(net_revenue) / int(month_count)
 
-# List the date and amount for greatest increase of profit
+    # Find the greatest increase of profit
+        if int(row[1]) > greatest_inc:
+            greatest_inc = int(row[1])
+            # List the date and amount for greatest increase of profit        
+            greatest_inc_month = row[0]
 
-# Find the greatest decrease of profit
+    # Find the greatest decrease of profit
+        if int(row[1]) < greatest_dec:
+            greatest_dec = int(row[1])
+            # List the date and amount for greatest decrease of profit
+            greatest_dec_month = row[0]
 
-# List the date and amount for greatest decrease of profit
+    highest_rev = max(month_change)
+    lowest_rev = min(month_change)
+
+#print statements
+print(f"Financial Analysis")
+print(f"------------------------")
+print(f"Total Months: {month_count}")
+print(f"Total: ${net_revenue}")
+print(f"Average Change: {average_change}")
+print(f("Greatest Inc in Profits: {greatest_inc_month}, {highest_rev}"))
+print(f("Greatest Dec in Profits: {greatest_dec_month}, {lowest_rev}"))
